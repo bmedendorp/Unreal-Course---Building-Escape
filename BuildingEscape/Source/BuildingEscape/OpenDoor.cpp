cd7382fae3f047467076c8df2f5c6564db0f3f38
Brian Medendorp <brian@medendorponline.com>
2016-11-20 19:26:16 -0800

    BE12 Moving Objects With C++

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,41 @@
+// Copyright Brian Medendorp 2016
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Get our owner
+	AActor* Owner = GetOwner();
+
+	// Setup a rotation
+	FRotator NewRotation = FRotator(0.0f, -70.0f, 0.0f);
+
+	// Set the new rotation
+	Owner->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
